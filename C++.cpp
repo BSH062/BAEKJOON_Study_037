5cd3c5bf3c2194ea797c2691c841c4e2596fe868
DESKTOP-NFUG9C6\tjrgu <bshpublic062@gmail.com>
2023-01-27 18:20:16 +0900

    2023/01/27 백준 문제 풀이  (손익분기점)

---
+++ C++.cpp
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main()
+{
+	int a, b, c;
+
+	scanf("%d %d %d", &a, &b, &c);
+	//가변비용보다 판매가격이 작거나 같으면 -1 
+	if (b >= c)
+	{
+		printf("-1");
+	}
+	else
+	{
+		printf("%d", a / (c - b) + 1);
+	}
+
+}
---
+++ C_sharp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int[] rel = Array.ConvertAll(input, int.Parse);
+
+            if (rel[1] >= rel[2])
+            {
+                Console.WriteLine("-1");
+            }
+            else
+            {
+                int eventPoint = rel[0] / (rel[2] - rel[1]) + 1;
+                Console.WriteLine(eventPoint);
+            }
+        }
+    }
+}
